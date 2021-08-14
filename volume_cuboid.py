ef371512e8d235ebe734485bb52da8c564f3dca2
mprabhakarraju <manoj.prabhakarraju@altran.com>
2021-08-14 03:11:10 +0100

    Add a python file

---
+++ volume_cuboid.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file for learning unit test.
+
+To find the Volume of a cuboid.
+
+@Manoj Prabhakar Raju
+
+"""
+
+def _volume_of_cuboid(_l):
+    return _l*_l*_l
+
+def _add(_l):
+    return _l+_l
+    
